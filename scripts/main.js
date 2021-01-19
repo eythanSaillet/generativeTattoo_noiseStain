6b79fd797ebc7328d0cef40cd72a69adfb66d453
eythanSaillet <eythan.saillet@gmail.com>
2021-01-19 22:59:47 +0100

    :sparkles: add datGui interface

--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,79 @@
 function setup() {
-	createCanvas(500, 100).parent('canvasContainer')
 	angleMode(DEGREES)
 	frameRate(60)
-	background(200)
 
 	system.init()
-	filter(BLUR, 13)
-	system.reDraw()
 }
 
 let system = {
 	// SYSTEM PROPERTIES
-	canvasWidth: null,
-	canvasHeight: null,
+	canvas: null,
+	imgWidth: null,
+	imgHeight: null,
 
-	noiseSensitivity: 0.45,
-	blurSensitivity: 0.6,
+	noiseSensitivity: 0.4,
+	blurSensitivity: 13,
 
 	// NOISE PROPERTIES
-	noiseScale: 0.025,
+	noiseScale: 0.02,
 
 	init() {
-		this.canvasWidth = width
-		this.canvasHeight = height
+		this.imgWidth = 300
+		this.imgHeight = 150
 
+		datGuiSetup()
+
+		system.generate()
+	},
+
+	generate() {
+		this.canvas = createCanvas(this.imgWidth, this.imgHeight).parent('#canvasContainer')
 		this.drawPoint()
+		filter(BLUR, this.blurSensitivity)
+		loadPixels()
+		this.reDraw()
 	},
 
 	drawPoint() {
 		let simplex = new SimplexNoise()
 		strokeWeight(1)
-		for (let i = 0; i < this.canvasHeight; i++) {
-			for (let j = 0; j < this.canvasWidth; j++) {
+		for (let i = 0; i < this.imgWidth; i++) {
+			for (let j = 0; j < this.imgHeight; j++) {
 				simplex.noise2D(j * this.noiseScale, i * this.noiseScale) + 0.5 > this.noiseSensitivity ? stroke(0) : stroke(255)
-				// noise(j * this.noiseScale, i * this.noiseScale) > this.noiseSensitivity ? stroke(0) : stroke(255)
-				point(j, i)
+				point(i, j)
 			}
 		}
 	},
 
 	reDraw() {
-		let black = color(0)
-		let white = color(255)
-		for (let i = 0; i < this.canvasHeight; i++) {
-			for (let j = 0; j < this.canvasWidth; j++) {
-				let pixelColor = get(j, i)[0]
+		for (let i = 0; i < this.imgWidth; i++) {
+			for (let j = 0; j < this.imgHeight; j++) {
+				let pixelColor = get(i, j)[0]
 				if (pixelColor > 127) {
-					set(j, i, white)
+					stroke('white')
 				} else {
-					set(j, i, black)
+					stroke('black')
 				}
+				point(i, j)
 			}
 		}
-		updatePixels()
+	},
+
+	save() {
+		saveCanvas('noiseStain', 'png')
 	},
 }
 
 function draw() {}
+
+function datGuiSetup() {
+	let gui = new dat.GUI({ width: 410 })
+
+	gui.add(system, 'imgWidth', 0, window.innerWidth).name('Longueur')
+	gui.add(system, 'imgHeight', 0, window.innerHeight).name('Largeur')
+	gui.add(system, 'noiseScale', 0, 0.05).name('Densité')
+	gui.add(system, 'blurSensitivity', 0, 50).name('Lissage')
+
+	gui.add(system, 'save').name('Sauvegarder')
+	gui.add(system, 'generate').name('Générer')
+}
